aa66e26520b802337c61dbd485d2b6ce73bb99c8
quoc48 <thanh.quoc731@gmail.com>
2024-12-24 22:44:45 +0700

    adding code for training simple model

--- expreriment/train_simple_model.py
+++ expreriment/train_simple_model.py
@@ -21,5 +21,38 @@ from ml_editor.data_processing import (
     get_split_by_author,
     get_vectorized_inputs_and_label,
     train_vectorizer,
+    get_vectorized_series,
 )
 
+from ml_editor.model_v1 import get_model_probabilities_for_input_texts
+
+
+data_path = Path('data/writers.csv')
+df = pd.read_csv(data_path)
+df = format_raw_df(df.copy())
+
+df = df.loc[df["is_question"]].copy()
+
+df = add_text_features_to_df(df.copy())
+train_df, test_df = get_split_by_author(df, test_size=0.2, random_state=40)
+
+vectorizer = train_vectorizer(train_df)
+train_df["vectors"] = get_vectorized_series(train_df["full_text"].copy(), vectorizer)
+test_df["vectors"] = get_vectorized_series(test_df["full_text"].copy(), vectorizer)
+
+features = [
+                "action_verb_full",
+                "question_mark_full",
+                "text_len",
+                "language_question",
+            ]
+X_train, y_train = get_feature_vector_and_label(train_df, features)
+X_test, y_test = get_feature_vector_and_label(test_df, features)
+
+clf = RandomForestClassifier(n_estimators=100, class_weight='balanced', oob_score=True)
+clf.fit(X_train, y_train)
+
+y_predicted = clf.predict(X_test)
+y_predicted_proba = clf.predict_proba(X_test)
+
+y_train.value_counts()
